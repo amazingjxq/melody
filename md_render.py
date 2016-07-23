dc9f746cffcfb8606a3a2039db262e9b4661b0ef
amazingjxq <amazingjxq@gmail.com>
2016-07-23 23:10:40 +0800

    MDRender class

--- md_render.py
+++ md_render.py
@@ -9,6 +9,52 @@ import markdown
 from jinja2 import Environment, PackageLoader
 import os
 import shutil
+from pygments.styles import get_style_by_name
+from pygments.styles import get_all_styles
+from pygments.formatters import HtmlFormatter
+
+
+class MDRenderException(Exception):
+    pass
+
+
+class MDRender:
+    def __init__(self, markdown_file):
+        "docstring"
+        self.markdown_file = markdown_file
+        self.extensions = [
+            "markdown.extensions.codehilite",
+            "markdown.extensions.tables",
+            "markdown.extensions.fenced_code"]
+        self.env = Environment(loader=PackageLoader('md_render', 'templates'))
+        self.templates = {
+            'github': 'github.html',
+        }
+        self.styles = list(get_all_styles())
+        self.rendered_html = ''
+
+    def render(self, template, style):
+        if template not in self.templates:
+            raise MDRenderException('Invalid template')
+
+        if style not in self.styles:
+            raise MDRenderException('Invalid style')
+
+        with open(self.markdown_file) as md_fd:
+            md_content = md_fd.read().decode("utf-8")
+            html_content = markdown.markdown(md_content,
+                                             extensions=self.extensions)
+            tpl = self.env.get_template(self.templates[template])
+
+            code_css = HtmlFormatter(style=style).get_style_defs()
+
+            self.rendered_html = tpl.render(md_html=html_content,
+                                            code_css=code_css).encode("utf-8")
+            return self.rendered_html
+
+    def export(self):
+        pass
+
 
 
 def render(md_content):
@@ -18,7 +64,7 @@ def render(md_content):
                                          "markdown.extensions.fenced_code"])
 
     env = Environment(loader=PackageLoader('md_render', 'templates'))
-    tpl = env.get_template('template.html')
+    tpl = env.get_template('github.html')
     return tpl.render(md_html=html)
 
 
@@ -51,16 +97,18 @@ def write_html(md_path, html_path, html_content):
 
 
 def process(md_path, html_path):
-    md_fd = open(md_path, 'r')
-    md_content = md_fd.read().decode("utf-8")
+    # md_fd = open(md_path, 'r')
+    # md_content = md_fd.read().decode("utf-8")
 
-    html_content = render(md_content)
+    # html_content = render(md_content)
 
-    create_dir(html_path)
+    # create_dir(html_path)
 
-    write_html(md_path, html_path, html_content)
+    # write_html(md_path, html_path, html_content)
 
-    copy_css(html_path)
+    # copy_css(html_path)
+    render = MDRender(md_path)
+    print render.render('github', 'tango')
 
 
 def main():
